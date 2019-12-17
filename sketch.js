3dc97b2a4bf7f73cf7dbb96ec1a4b8d9a4241a42
mkolany <58729851+mkolany@users.noreply.github.com>
2019-12-17 14:54:50 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,132 +1,73 @@
 /*
-* @name Drawing
-* @description Generative painting program.
+* @name Lorenz Attractor
+* @description Generative painting of a Lorenz's strange attractor.
 */
 
-// All the paths
-let paths = [];
-// Are we painting?
-let painting = false;
-// How long until the next circle
-let next = 0;
-// Where are we now and where were we?
-let current;
-let previous;
+// ode init
+let x = 0;
+let y = 1;
+let z = 5;
 
-function setup() {
-  createCanvas(720, 400);
-  current = createVector(0,0);
-  previous = createVector(0,0);
-};
+let a = 10;
+let b = 28;
+let c = 8.0 / 3.0;
 
-function draw() {
-  background(200);
-  
-  // If it's time for a new point
-  if (millis() > next && painting) {
+// simulation settings
+var speed = 50;
+var step = 0.0005;
+var fade_alpha = 5;
 
-    // Grab mouse position      
-    current.x = mouseX;
-    current.y = mouseY;
+// ui related stuff
+let button;
+let butToggle=false;
+let slider;
 
-    // New particle's force is based on mouse movement
-    let force = p5.Vector.sub(current, previous);
-    force.mult(0.05);
-
-    // Add new particle
-    paths[paths.length - 1].add(current, force);
+function doShit(iters){
+  if (butToggle){
+  for( let i = 0; i < iters; i++) {
+    let dt = step;
+    let dx = a * (y - x) * dt;
+    let dy = (x * (b - z) - y) * dt;
+    let dz = (x * y - c * z) * dt;
+    x = x + dx;
+    y = y + dy;
+    z = z + dz;
     
-    // Schedule next circle
-    next = millis() + random(100);
+    x0=map(x,-25,25,0,width/2);
+    y0=map(y,-30,30,0,height/2);
+    z0=map(z,-18,48,0,height/2);
 
-    // Store mouse values
-    previous.x = current.x;
-    previous.y = current.y;
+    point(x0,y0);
+    point(x0+width/2,z0);
+    point(y0,z0+height/2-10);
+    fill(0,fade_alpha);
   }
-
-  // Draw all paths
-  for( let i = 0; i < paths.length; i++) {
-    paths[i].update();
-    paths[i].display();
+  rect(-1,-1,width+2,height+2); //fade
   }
 }
 
-// Start it up
-function mousePressed() {
-  next = 0;
-  painting = true;
-  previous.x = mouseX;
-  previous.y = mouseY;
-  paths.push(new Path());
-}
-
-// Stop
-function mouseReleased() {
-  painting = false;
-}
-
-// A Path is a list of particles
-class Path {
-  constructor() {
-    this.particles = [];
-    this.hue = random(100);
-  }
-
-  add(position, force) {
-    // Add a new particle with a position, force, and hue
-    this.particles.push(new Particle(position, force, this.hue));
-  }
+function setup(){
+  createCanvas(600,600);
+  background(0);
   
-  // Display plath
-  update() {  
-    for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].update();
-    }
-  }  
+  // ui
+  button = createButton('start/stop');
+  button.position(10, 10);
+  button.mousePressed(toggleFun);
   
-  // Display plath
-  display() {    
-    // Loop through backwards
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      // If we shold remove it
-      if (this.particles[i].lifespan <= 0) {
-        this.particles.splice(i, 1);
-      // Otherwise, display it
-      } else {
-        this.particles[i].display(this.particles[i+1]);
-      }
-    }
-  
-  }  
+  slider = createSlider(0.1, 500, speed);
+  slider.position(100, 10);
+  slider.style('width', '80px');
+  }
+
+function toggleFun(){
+  butToggle = !butToggle;
 }
 
-// Particles along the path
-class Particle {
-  constructor(position, force, hue) {
-    this.position = createVector(position.x, position.y);
-    this.velocity = createVector(force.x, force.y);
-    this.drag = 0.95;
-    this.lifespan = 255;
-  }
-
-  update() {
-    // Move it
-    this.position.add(this.velocity);
-    // Slow it down
-    this.velocity.mult(this.drag);
-    // Fade it out
-    this.lifespan--;
-  }
-
-  // Draw particle and connect it with a line
-  // Draw a line to another
-  display(other) {
-    stroke(0, this.lifespan);
-    fill(0, this.lifespan/2);    
-    ellipse(this.position.x,this.position.y, 8, 8);    
-    // If we need to draw a line
-    if (other) {
-      line(this.position.x, this.position.y, other.position.x, other.position.y);
-    }
-  }
+function draw(){
+  stroke(255,500);
+  speed = slider.value();
+  
+  // noprotect
+  doShit(speed);
 }
